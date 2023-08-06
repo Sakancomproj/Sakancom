d722e1cd7b49371b81c1841d741880bef19164aa
Jana <Jana@DESKTOP-4QB8764>
2023-08-06 23:22:32 +0200

    version 1

--- src/main/java/jana/Database.java
+++ src/main/java/jana/Database.java
@@ -327,7 +327,8 @@ List <String> pass=new ArrayList <String>();
 					 if(!(mail.contains("@"))) {
 						f4=true;
 						logger.info("The email you entered is wrong");
-					} if(!(phon.length()==10 || phon.length()>10)) {
+					} 
+					 if(!(phon.length()==10 || phon.length()>10)) {
 						logger.info("The phone number you entered is incorrect");
 						f4=true;
 					}
@@ -364,37 +365,20 @@ List <String> pass=new ArrayList <String>();
 					dbuser.get(i).log=true;
 				}}	
 			logger.info("Your information has been modified ,this is your new information:");
-			for (int i = 0; i < dbuser.size(); i++) {
-			    if (dbuser.get(i).id.equals(id2)) {
-			        logger.info("name: ");
-			        logger.info(dbuser.get(i).name);
-
-			        // Check if phone number exists and log it
-			        if (dbuser.get(i).phone != null) {
-			            logger.info("phone:");
-			            logger.info(dbuser.get(i).phone);
-			        }
-
-			        // Check if email exists and log it
-			        if (dbuser.get(i).email != null) {
-			            logger.info("email:");
-			            logger.info(dbuser.get(i).email);
-			        }
-
-			        // Check if address exists and log it
-			        if (dbuser.get(i).address != null) {
-			            logger.info("address:");
-			            logger.info(dbuser.get(i).address);
-			        }
-
-			        // Check if password exists and log it
-			        if (dbuser.get(i).password != null) {
-			            logger.info("password:");
-			            logger.info(dbuser.get(i).password);
-			        }
-			    }
-			}
-	 
+			for(int i=0;i< dbuser.size();i++ ) {
+				if(dbuser.get(i).id.equals(id2)){
+					logger.info("name: ");
+					logger.info(dbuser.get(i).name);
+					logger.info(" phone:");
+					logger.info(dbuser.get(i).phone);
+					logger.info(" email:");
+					logger.info(dbuser.get(i).email);
+					logger.info(" address:");
+					logger.info(dbuser.get(i).address);
+					logger.info(" password:");
+					logger.info(dbuser.get(i).password);
+					
+				}}	 
 			
 		}
 	 
