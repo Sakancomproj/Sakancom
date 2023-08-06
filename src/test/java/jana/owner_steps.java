693b347a47c430393d7ebc86eae29afab74ee2df
Jana <Jana@DESKTOP-4QB8764>
2023-08-06 16:07:45 +0200

    version 1

--- src/test/java/jana/owner_steps.java
+++ src/test/java/jana/owner_steps.java
@@ -12,6 +12,9 @@ public class owner_steps {
 	Database db =new Database ();
 	Owner ow=new Owner();
 	boolean log_ow;
+boolean flag_created;
+	
+	boolean flag_email;
 @Given("that the owner with email {string} and pass {string} want to log in to the application")
 public void thatTheOwnerWithEmailAndPassWantToLogInToTheApplication(String string, String string2) {
 	Owner.checkifexi8( string,  string2);
@@ -34,5 +37,23 @@ public void theOwnerLoggingInFailed() {
 	assertTrue(log_ow);
 }
 
+@Given("that the owner with email {string} is log in")
+public void thatTheOwnerWithEmailIsLogIn(String string) {
+	flag_created=false;
+	
+	 
+	flag_email=db.checkemail(string);
+}
+
+@Given("there is an apartment  with location {string} ,capacity {string},picture {string}, description {string}")
+public void thereIsAnApartmentWithLocationCapacityPictureDescription(String string, String string2, String string3, String string4) {
+	db.createapartment( string, string2, string3, string4,100);
+	flag_created=true;
+}
+
+@Then("the  apartment  with location {string} ,capacity {string},picture {string}, description {string} is created successfully")
+public void theApartmentWithLocationCapacityPictureDescriptionIsCreatedSuccessfully(String string, String string2, String string3, String string4) {
+	 assertTrue(flag_created);
+}
 
 }
--- use_cases/owner.feature
+++ use_cases/owner.feature
@@ -12,3 +12,8 @@ Then The owner is logged in successfully
 Given that the owner with email "deema@gmail.com" and pass "deema123" want to log in to the application
 When  the owner with email "deema@gmail.com" and pass "raya123" logs in to the application
 Then The owner logging in failed 
+
+Scenario: Create new apartment
+Given  that the owner with email "deema@gmail.com" is log in 
+And there is an apartment  with location "Rafedia" ,capacity "Three",picture "c5", description "three beds,bathroom and kitchen" 
+Then the  apartment  with location "Rafedia" ,capacity "Three",picture "c5", description "three beds,bathroom and kitchen" is created successfully
